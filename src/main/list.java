00fcf98e7d01f43c1b379c2ff5296db642def8b5
4epic <AndreyNovitski@mail.ru>
2020-11-13 13:38:15 +0300

    first commit

---
+++ src/main/list.java
@@ -0,0 +1,110 @@
+package main;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class list {
+    public static void main(String[] args) throws FileNotFoundException {
+        ArrayList<String> laba8 = new ArrayList<String>();
+        laba8.add(0,"laba8");
+
+        int choice;
+
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Меню");
+
+        System.out.println("1 Добавить");
+        System.out.println("2 Удалить");
+        System.out.println("3 Поиск одинаковых элементов");
+        System.out.println("4 Реверс всех строк");
+        System.out.println("5 Статистика по символам");
+        System.out.println("6 Поиск подстроки в строках");
+        System.out.println("7 Считать текстовый файл");
+        System.out.println("8 Длины строк");
+
+        System.out.println("Введите ваш выбор");
+
+        do {
+            choice=scan.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Введите новый элемент");
+                    Scanner novoe = new Scanner(System.in);
+                    String addtext = novoe.nextLine();
+                    laba8.add(1, addtext);
+                    System.out.println(laba8);
+                    break;
+
+                case 2:
+                    System.out.println("Введите индекс элемента , который хотите удалить");
+                    Scanner del = new Scanner(System.in);
+                    int del1 = del.nextInt();
+                    laba8.remove(del1);
+                    System.out.println(laba8);
+                    break;
+
+                case 3:
+                    HashMap<String,Integer> hm = new HashMap<String, Integer>();
+                    Integer am;
+                    for(String i: laba8){
+                        am=hm.get(i);
+                        hm.put(i,am==null? 1:am+1);
+                    }
+                    System.out.println(hm);
+                    break;
+
+                case 4:
+                    Collections.reverse(laba8);
+                    System.out.println(laba8);
+                    break;
+
+                case 5:
+                    String laba8tr=laba8.toString();
+                    Map<Character,Integer> map = new TreeMap<Character, Integer>();
+                    for (char c : laba8tr.toCharArray())
+                        map.put(c,(map.containsKey(c))?map.get(c)+1:1);
+
+                    StringBuilder sb=new StringBuilder();
+                    for (char c:map.keySet())
+                        sb.append(c);
+                    System.out.println("Имеются символы \""+sb.toString()+"\"");
+
+                    for (char c:map.keySet())
+                        System.out.printf("Количество '%c'=%d \n",c,map.get(c));
+                    break;
+
+                case 6:
+                    System.out.println("Введите подстроку");
+                    Scanner dev=new Scanner(System.in);
+                    String podstr=dev.nextLine();
+
+                        if(laba8.get(1).contains(podstr))
+                        System.out.println("Подстрока найдена в строке "+laba8.get(1));
+
+                        if(laba8.get(2).contains(podstr))
+                        System.out.println("Подстрока найдена в строке " +laba8.get(2));
+
+                        if(laba8.get(3).contains(podstr))
+                        System.out.println("Подстрока найдена в строке " + laba8.get(3));
+                    break;
+
+                case 7:
+                    Scanner s =new Scanner(new File("C:\\Users\\Andrey\\Desktop\\Git\\text.txt"));
+                    String bla=s.nextLine();
+                    laba8.add(bla);
+                    System.out.println(laba8);
+                    break;
+
+                case 8:
+                    System.out.println("Длина первой строки = "+laba8.get(1).length());
+                    System.out.println("Длина второй строки = "+laba8.get(2).length());
+                    System.out.println("Длина третьей строки = "+laba8.get(3).length());
+                    break;
+
+                default:
+                    System.out.println("Неверный ввод");
+            }
+        }while (choice!=15);
+    }
+}
